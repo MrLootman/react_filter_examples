879fda6bdd5774f56a1ff93d3dc6884b089eb9f0
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2023-04-19 17:52:28 +0200

    🐛 Input perméable aux lettres en minuscule

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,26 +5,11 @@ function HomePage() {
   const [data, setData] = useState([]); // Ce state sert à contenir les données en provenance de l'API.
   const [inputValue, setInputValue] = useState(""); // Ce state sera connecté à l'input, pour changer dynamiquement sa valeur.
 
-  const [laGueuleDeLapi, setLaGueuleDeLapi] = useState([]);
-
-  useEffect(() => {
-    fetch("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002?appid=1929610")
-      .then((res) => res.json())
-      .then((data) => {
-        setLaGueuleDeLapi(data)
-      })
-      .then(() => console.log(laGueuleDeLapi))
-      .catch((err) => console.error(err))
-  })
-
   function handleChange(e) {
     setInputValue(e.target.value) // La fonction handleChange permet de mettre à jour inputValue à chaque caractère ajouté ou supprimé dans l'input.
   }
 
-  // useEffect est utilisé pour effectuer des effets secondaires contrôlés dans les composants React, 
-  // comme les requêtes à l'API. Cela permet de s'assurer que le comportement du composant est 
-  // cohérent et prévisible, même lorsqu'il y a des changements dans les variables d'état ou les propriétés.
-  useEffect(() => { // On utilise un useEffect car veut que la variable data soit re-rendue une fois qu'elle sera mise à jour.
+  useEffect(() => { // On utilise un useEffect car veut que la variable nommée data soit re-rendue une fois qu'elle sera mise à jour.
     fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
       .then((data) => {
@@ -36,15 +21,14 @@ function HomePage() {
   return (
     <div className="home-page">
       <h1>Rick and Morty</h1>
-      <input 
+      <input
         value={inputValue}
         onChange={(e) => handleChange(e)} 
       />
-
       <ul>
         {
           data
-            .filter((el) => el.name.includes(inputValue))
+            .filter((el) => el.name.toLowerCase().includes(inputValue.toLowerCase()))
             .map((element) => {
             return (
               <RickAndMortyCard name={element.name} image={element.image} />
